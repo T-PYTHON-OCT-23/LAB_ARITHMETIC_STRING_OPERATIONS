3b328f03d8ef906788a001fedc18aea2c4191611
ebtesam <ebtesam167@hotmail.com>
2023-10-11 11:30:15 +0300

    solved lab_ARITHMETIC

--- lab2Operations.py
+++ lab2Operations.py
@@ -5,12 +5,9 @@ tax_rate = 7.5
 print("Price of item: $" + str(price))
 print("Quantity: " + str(quantity))
 print("Tax rate: " + str(tax_rate) + "% \n" )
+
 subtotal = price * quantity 
-print("Subtotal: $" + str(round(subtotal, 2)))
-
 tax = subtotal * 0.075
-print("Tax: $" + str(round(tax, 2)))
-
-
 total = subtotal + tax
-print("Total: $" + str(round(total, 2)))
+output = f"Subtotal: ${str(round(subtotal, 2))}\nTax: ${str(round(tax, 2))} \nTotal: ${str(round(total, 2))} "
+print(output)
