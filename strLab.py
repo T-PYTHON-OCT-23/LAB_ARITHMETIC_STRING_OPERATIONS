31fb83f6d230ca66adf439fef92edeeb65025fb7
m7md <m7md.husinn@gmail.com>
2023-10-11 12:10:29 +0300

    Modify the solution

--- strLab.py
+++ strLab.py
@@ -3,16 +3,16 @@ sentence = "I think we are going to have a good day"
 #Define a string variable containing a word that appears in the sentence.
 word_from_sentence = "good"
 #Print the length of the sentence.
-print(len(sentence))
+print(f"length of the sentence: {len(sentence)}")
 #Print the index of the first occurrence of the word in the sentence.
-print(sentence[0])
+print(f"the first occurrence of the word in the sentence: {sentence.find(word_from_sentence)}")
 #Print the number of times the word appears in the sentence.
-print(sentence.count(word_from_sentence))
+print(f"the number of times the word appears in the sentence: {sentence.count(word_from_sentence)} " + "times")
 # Print the sentence in all uppercase letters.
-print(sentence.upper())
+print("the sentence in all uppercase letters: "+ sentence.upper())
 #Print the sentence in all lowercase letters.
-print(sentence.lower())
+print("the sentence in all lowercase letters: " +sentence.lower())
 #Replace the word in the sentence with a new word of your choice
-print(sentence.replace(word_from_sentence , "bad"))
+print(f"Reblasce th word (good) with the word (bad): {sentence.replace( word_from_sentence , "bad")}")
 #Print the last character of the sentence.
-print(sentence[-1])
+print("the last character of the sentence: "+ sentence[-1])
