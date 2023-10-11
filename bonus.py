045a5ba4408da2b59cd34f99e74f8cb8370554a3
AlenaziHazal <roomyham@MacBook-Pro.local>
2023-10-11 11:06:19 +0300

    Bonus is solved

---
+++ bonus.py
@@ -0,0 +1,10 @@
+name = "Hazal Hamad is the greates man in earth and most heartless man"
+family_name = name + " Alenazi"
+family_name_count = len(family_name)
+print(f"{family_name_count}")
+print(f"{family_name[0]}")
+print(f"{family_name.count('a')}")
+print(f"{name.upper()}")
+print(f"{name.lower()}")
+print(f"{name.replace('heartless','kindness')}")
+print(f"{name[-1]}")
