05d686d454070c2f4c0a73401159116620ab8361
abdullah <abdullah.alanezi.23@gmail.com>
2023-10-11 11:18:44 +0300

    Bonus lab

---
+++ Bonus.py
@@ -0,0 +1,13 @@
+
+story="i am abdullah mofarih,i live in hail city, i have bachelor's degree in software engineering"
+
+name="abdullah"
+
+print(len(story))
+print(name[0])
+print(story.count(name))
+print(story.upper())
+print(story.lower())
+story_ubdate = story.replace('mofarih','alanezi')
+print(story_ubdate)
+print(story[-1])
