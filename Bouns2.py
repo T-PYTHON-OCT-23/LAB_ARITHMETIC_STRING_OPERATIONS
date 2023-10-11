2857a29d0375a5143287f53f8534a11eb755289b
AseelAlshammrani <AseelAlshammrani@gmail.com>
2023-10-11 13:05:44 +0300

    Done

---
+++ Bouns2.py
@@ -0,0 +1,11 @@
+text='I am Aseel Alshammrani, and my age is 23. Also, my major is IS'
+word='Aseel'
+print('The length of the sentence is',len(text))
+print(f"The location of {word} in the phrase is in",text.find(word))
+print('the number of times the {} appears'.format(text.count(word)))
+print('Print the sentence in all uppercase letters: ',text.upper())
+print('Print the sentence in all lowercase letters: ',text.lower())
+print('Replace the word in the sentence with a new word of your choice: ',text.replace('Aseel','Remas'))
+print('rint the last character of the sentence: ',text[-1])
+
+
